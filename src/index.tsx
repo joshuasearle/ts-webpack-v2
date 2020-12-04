4a1056ec2aafb599c2803dddf18091cbbc6fbfbb
joshuasearle <joshryansearle@gmail.com>
2020-12-04 22:11:11 +1100

    Add css sheet

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import './css/main.css';
+
 const root = document.querySelector('#root');
 
 ReactDOM.render(<h1>Hello, World!!!</h1>, root);
